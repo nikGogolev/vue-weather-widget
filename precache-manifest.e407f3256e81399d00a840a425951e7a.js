085fff546cb1296325a3ab9c556864d5e8549e8e
nikGogolev <nv_gogolev@mail.ru>
2022-09-04 10:15:20 +0300

    deploy

---
+++ precache-manifest.e407f3256e81399d00a840a425951e7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5148ff691adfda069858",
+    "url": "/vue-weather-widget/css/app.c768e8ea.css"
+  },
+  {
+    "revision": "1dd5d3eed833e3bf8f816b200a5e3fd4",
+    "url": "/vue-weather-widget/index.html"
+  },
+  {
+    "revision": "5148ff691adfda069858",
+    "url": "/vue-weather-widget/js/app.b2890b94.js"
+  },
+  {
+    "revision": "79e60506629fe79dfadd",
+    "url": "/vue-weather-widget/js/chunk-vendors.38a48b78.js"
+  },
+  {
+    "revision": "c7d03da9c5c4185b10da7e282aff5532",
+    "url": "/vue-weather-widget/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-weather-widget/robots.txt"
+  }
+]);
